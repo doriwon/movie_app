a7914e6c8243f59cf08c7d40d74de63bad90665d
unknown <wldnjs7204@naver.com>
2021-03-21 00:29:10 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,51 +1,26 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
 import "./App.css";
 
-/* Class Component */
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: []
-  };
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies }
-      }
-    } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
-    this.setState({ movies, isLoading: false }) // (setState에서의)movies:(axios에서의)movies
-  }
-  componentDidMount() {
-    this.getMovies();
-  };
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader_text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map(movie => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      {/* <Route path="/" exact={true} component={Home} >
+        <h1>Home</h1>
+      </Route>
+      <Route path="/about" component={About}>
+        <h1>About</h1>
+      </Route> */}
+      <Route path="/" exact={true} component={Home} /> {/* exact={true} : 오로지 이것만 랜더링함 */}
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
+  );
 }
-export default App;
 
+export default App;
